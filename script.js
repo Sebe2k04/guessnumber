446c08d5f5444b28c42a7cb2748497fd605be68b
sebe <sebe2k04@gmail.com>
2024-01-02 16:33:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var ans=Math.floor(Math.random()*10);
+        var result=document.getElementById("result");
+        console.log(ans);
+        var score=10;
+        function check()
+        {
+            var num=document.getElementById("num").value;
+            num=Number(num);
+            if(num==ans)
+            {
+                result.textContent="WIN";
+                document.getElementById("score").innerHTML=Number(score);
+            }
+            else{
+                result.textContent="WRONG"
+                score=score-1;
+                document.getElementById("score").innerHTML=Number(score);
+                console.log(score)
+            }
+        }
